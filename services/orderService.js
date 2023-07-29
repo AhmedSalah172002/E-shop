9f1d750d59e409699af99ca46c67950b04bbf100
AhmedSalah172002 <ahmed172002tayel.com>
2023-07-29 21:53:12 +0300

    first commit6

--- services/orderService.js
+++ services/orderService.js
@@ -143,13 +143,9 @@ exports.checkoutSession = asyncHandler(async (req, res, next) => {
   const session = await stripe.checkout.sessions.create({
     line_items: [
       {
-        price_data: {
-          currency: 'egp',
-          unit_amount: totalOrderPrice * 100,
-          product_data: {
-            name: req.user.name,
-          },
-        },
+        name: req.user.name,
+        amount_total: totalOrderPrice * 100,
+        currency: 'egp',
         quantity: 1,
       },
     ],
@@ -158,9 +154,9 @@ exports.checkoutSession = asyncHandler(async (req, res, next) => {
     cancel_url: `${req.protocol}://${req.get('host')}/cart`,
     customer_email: req.user.email,
     client_reference_id: req.params.cartId,
-    metadata: req.body.shippingAddress ,
+    metadata: req.body.shippingAddress,
   });
-  
+
   // 4) send session to response
   res.status(200).json({ status: 'success', session });
 });
